33b1895e333b479ab04d9f0de4729ae43bc28461
Matthew <turbodieseldodge@gmail.com>
2023-02-06 18:22:17 -0600

    side effects

--- 05_side_effects_and_data_fetching/project_showcase/src/App.js
+++ 05_side_effects_and_data_fetching/project_showcase/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import Header from "./components/Header";
 import ProjectForm from "./components/ProjectForm";
@@ -7,29 +7,54 @@ import ProjectList from "./components/ProjectList";
 const App = () => {
   const [projects, setProjects] = useState([]);
   const [isDarkMode, setIsDarkMode] = useState(true);
+  const [selectedPhase, setSelectedPhase] = useState("");
+  const [searchQuery, setSearchQuery] = useState("");
 
   const onAddProject = (newProject) => {
-    setProjects(projects => {
-      return [...projects, newProject]
-    })
-  }
+    setProjects((projects) => {
+      return [...projects, newProject];
+    });
+  };
 
   const onToggleDarkMode = () => {
-    setIsDarkMode(isDarkMode => !isDarkMode)
-  }
+    setIsDarkMode((isDarkMode) => !isDarkMode);
+  };
 
-  const loadProjects = () => {
-    fetch("http://localhost:4000/projects")
+  useEffect(() => {
+    console.log("side effect");
+    let url;
+    if (selectedPhase && searchQuery) {
+      url =
+      `http://localhost:4000/projects?phase=${selectedPhase}&
+       q=${encodeURI(searchQuery)}`
+    } else if (searchQuery) {
+      url =
+      `http://localhost:4000/projects?q=${encodeURI(selectedPhase)}`
+    } else if (selectedPhase) {
+      url =
+      `http://localhost:4000/projects?phase=${selectedPhase}`
+    } else {
+      url =
+      "http://localhost:4000/projects"
+    }
+
+    fetch(url)
       .then((res) => res.json())
       .then((projects) => setProjects(projects));
-  }
+  }, [selectedPhase, searchQuery]);
+
+  console.log("rendering");
 
   return (
     <div className={isDarkMode ? "App" : "App light"}>
       <Header isDarkMode={isDarkMode} onToggleDarkMode={onToggleDarkMode} />
-      <ProjectForm onAddProject={onAddProject}  />
-      <button onClick={loadProjects}>Load Projects</button>
-      <ProjectList loadProjects={loadProjects} projects={projects} />
+      <ProjectForm onAddProject={onAddProject} />
+      <ProjectList
+        projects={projects}
+        setSelectedPhase={setSelectedPhase}
+        searchQuery={searchQuery}
+        setSearchQuery={setSearchQuery}
+      />
     </div>
   );
 };
